b7eca2b1af290c2c245335f84b6cd1e7f7b39ede
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-10-18 05:54:39 -0500

    Funciones definidas y predefinidas

--- repaso1/funciones.py
+++ repaso1/funciones.py
@@ -1,5 +1,9 @@
+def funcionDefinida(): 
+    print("Soy una funcion definida")
+
 # Comentario de linea
 """
     comentario de bloque
 """
 print("Función predefinida,aquí es donde pondras tu hola mundo :v")
+funcionDefinida()
