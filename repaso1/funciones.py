e3d5e963be2110c0361fed91a86df6e2439b464d
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-10-18 20:28:50 -0500

    Se hizo pruebas en pycharm

--- repaso1/funciones.py
+++ repaso1/funciones.py
@@ -1,9 +1,7 @@
-def funcionDefinida(): 
-    print("Soy una funcion definida")
+def sumar(numero1,numero2):
+    return numero1+numero2
 
-# Comentario de linea
-"""
-    comentario de bloque
-"""
-print("Función predefinida,aquí es donde pondras tu hola mundo :v")
-funcionDefinida()
+print("Calculando la suma : ")
+numero1=input("Número 1 ")
+numero2=input("Número 2 ")
+print("La suma es  ",sumar(int(numero1),int(numero2)))
