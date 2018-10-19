0877065062390146665f400066582bcfda6a8d68
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-10-19 02:38:03 -0500

    Se creo al robot :v

---
+++ repaso1/Robot.py
@@ -0,0 +1,25 @@
+# vida --> 100 --> vivo 0
+# revivir ,dar comer, pegar -->-10
+
+def nacimento ():
+    global vida
+    vida = 100
+def matar():
+    global vida
+    vida=0
+def pegar():
+    global vida
+    vida-=10
+def alimentar():
+    global vida
+    vida+=10
+
+def revivir():
+    global vida
+    vida=100
+def laVida():
+    global vida
+    return vida
+nacimento()
+pegar()
+print(laVida())
