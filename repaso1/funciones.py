176a18e991909218a3f2a7ee4b78753c100eff14
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-10-18 20:31:37 -0500

    Tipos de datos

--- repaso1/funciones.py
+++ repaso1/funciones.py
@@ -5,3 +5,12 @@ print("Calculando la suma : ")
 numero1=input("Número 1 ")
 numero2=input("Número 2 ")
 print("La suma es  ",sumar(int(numero1),int(numero2)))
+
+# Tipos de datos 
+
+# Division 
+
+# Resultado decimal
+print(8/5)
+# Resultado entero
+print(8//5)
