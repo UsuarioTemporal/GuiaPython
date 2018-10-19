81e78afea71ca95346cf44c865c3b674db6bf787
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-10-19 02:01:13 -0500

    Funcional

--- repaso1/NumeroDeCasas.py
+++ repaso1/NumeroDeCasas.py
@@ -1,9 +1,12 @@
 import math
 numeroDeCasas=int(input("Ingrese el número de casas"))
 if numeroDeCasas < 8 :
-    print("No exite casa que cumpla la condición")
+    print("No exite casa que cumpla la condición : ")
 else :
     casa = (numeroDeCasas**2 + numeroDeCasas)/2 
-    casa = math.sqrt(casa) // 1
-    # si el número es entero has algo
-    print(casa)
+    valorEntero = int(math.sqrt(casa))
+    casa = math.sqrt(casa) 
+    if valorEntero == casa :
+        print("La casa encontrada es ",valorEntero)
+    else : 
+        print("No existe casa que cumpla la condición")
