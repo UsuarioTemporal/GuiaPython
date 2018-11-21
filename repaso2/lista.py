f9cc7641b38657875f5d24986019ae0ec64ef5cd
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-21 01:31:52 -0500

    Agregando el analisis de las listas

--- repaso2/lista.py
+++ repaso2/lista.py
@@ -1,4 +1,9 @@
-lista=["Texto 1","texto 2",15]
+lista=["Texto 1","texto 2",15,14]
+
+# Imprimer la lista completa
 print(lista)
 print(lista[:])
-print(lista[1:1])
+
+
+# desde 0 hast 2 pero este no lo toma es decir [0;2>
+print(lista[0:2])
