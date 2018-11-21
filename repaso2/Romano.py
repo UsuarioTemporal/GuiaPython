4da4b09f25681fdd6db601c5a3017e70b5766ec7
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-21 14:03:36 -0500

    Curiosidades

--- repaso2/Romano.py
+++ repaso2/Romano.py
@@ -1,5 +1,27 @@
-def convertir(lista) :
+def convertir(lista) : 
     print()
 
-lista = []
-lista=input("Ingresa el número a convertir :  ")
+
+
+
+
+lista = list(input("Ingrese un numero : "))
+convertir(lista)
+
+# # CURIOSIDADES
+
+# lista = []
+
+# lista.append("Hola")
+# lista=list("comida")
+# lista.append("todo")
+# print(lista)
+# # print(type(lista))
+
+
+# lista2 = list()
+# lista2=list(input("Ingrese un numero : "))
+# print(lista2)
+# # print(type(lista2))
+
+
