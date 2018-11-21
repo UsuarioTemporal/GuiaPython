9a84cd295b6e7752129552232431e672ef7f3ace
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-21 01:37:06 -0500

    Analisis de listas 2

--- repaso2/lista.py
+++ repaso2/lista.py
@@ -1,5 +1,5 @@
 lista=["Texto 1","texto 2",15,14]
-
+lista.append("Carlos")
 # Imprimer la lista completa
 print(lista)
 print(lista[:])
@@ -7,3 +7,5 @@ print(lista[:])
 
 # desde 0 hast 2 pero este no lo toma es decir [0;2>
 print(lista[0:2])
+print(lista[0:])
+print(lista[:3])
