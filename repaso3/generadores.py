4ea335ac47270aa9e3bf6981135823c8b8bd6160
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-21 22:20:22 -0500

    Agregando eficiencias de generadores

--- repaso3/generadores.py
+++ repaso3/generadores.py
@@ -13,9 +13,36 @@ def generarListaconPares(terminos) :
         if aleatorio %2 ==0 :
             lista.append(aleatorio)
             terminos-=1
-
+    
     return lista
             
 
 numeroTerminos = int(input("Ingrese el numero de terminos : "))
 print(generarListaconPares(numeroTerminos))
+
+
+
+
+"""
+    Los generadores son mas eficientes que las funciones tradicionales
+    muy utilies con listas de valores infinitos
+    bajo determinados escenarios , será muy util que un generador devuelva los
+    valores de uno en uno
+"""
+
+print("Generadores")
+def generarNumero(terminos) : 
+    lista = []
+    while terminos>0 :
+        aleatorio = random.randint(0,100)
+        if aleatorio %2 ==0 :
+            lista.append(aleatorio)
+            yield lista
+            terminos-=1
+    
+# Debo almacenarlo n un objeto iterable
+ObjetoIterable = generarNumero(10)
+
+for item in ObjetoIterable : 
+    print(item)
+
