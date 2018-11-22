49d9b452b56b72e6133fe8d444a36d3dd2a93e5c
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-22 01:44:09 -0500

    Generador enumerate

--- repaso3/generadores2.py
+++ repaso3/generadores2.py
@@ -2,11 +2,13 @@
     Los generadores en python nos van a servir para poder crer objetos sin necesidad de
     almacenarlos en la memoria ram los cuales se ven reflejados en codificaciones
     como el for ,donde encontramos el numero y el rango los cuales son un generador
-    
+
 
 """
 
-
+"""
+    ejemplos enumarete , range
+"""
 
 """ 
     cuando se utiliza yield from : se simplifica el código de los generadores en el caso de 
@@ -16,3 +18,12 @@
 x = (i for i in range(10))
 for item in x :
     print(item)
+
+for item in enumerate("abs"):
+    print(item)
+
+lista = ["perro","gato","rata","burro"]
+
+for x,y in enumerate(lista) : #x posicion e y valor
+    if x%2==0:
+        print(y)
