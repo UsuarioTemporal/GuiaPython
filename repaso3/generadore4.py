6558537964229140b510dcd1b73dbe32213061f1
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-04 01:00:25 -0500

    Corrigiento problema de sintaxis

--- repaso3/generadore4.py
+++ repaso3/generadore4.py
@@ -1,4 +1,4 @@
-class PrimerosN(Object) : 
+class PrimerosN(object) : 
     def __init__(self,terminos):
         self.terminos =terminos
         self.numero , self.listaNumeros = 0,[]
@@ -6,4 +6,9 @@ class PrimerosN(Object) :
     def __iter__(self): #funcion iterable
         return self
 
-    
+    #retornara el sigueinte 
+    def __next__(self) : 
+        return self.next()
+    def next(self) : 
+        if self.numero < self.terminos :
+            pass
