78b4aaffb7fb674b5050d6a0c95801d15f4a3807
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-04 01:32:01 -0500

    Terminando el tema de genradores a profundidad

---
+++ repaso3/generadores5.py
@@ -0,0 +1,15 @@
+lista = [2 * num for num in range(10)] 
+Objeto = (2 * num for num in range(10))
+
+#  Quizá en este ejemplo no veas la diferencia 
+# print('Lista')
+# for item in lista : 
+#     print(item)
+
+# print('\nObjeto')
+# for item in Objeto : 
+#     print(item)
+
+# Aquí esta lo mas bonito , TEORIA , todo es teoria
+print(type(lista))
+print(type(Objeto))
