f92f9cce8986b5b2cc0cdd45f92d9e1da345ab22
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-04 12:07:46 -0500

    Agregando nuevas fuentes

--- curiosidades/DoubleOSimples.py
+++ curiosidades/DoubleOSimples.py
@@ -1,4 +1,5 @@
 # He tenido problemas con el interprete al usar comillas dobles y aveces 
 # problemas al usar problemas simples 
 
-#fuente : https://recursospython.com/guias-y-manuales/comillas-dobles-o-simples/
+#fuentes : https://recursospython.com/guias-y-manuales/comillas-dobles-o-simples/
+#          http://recursospython.com/guias-y-manuales/zen-de-python-tim-peters/
