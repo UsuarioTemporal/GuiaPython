54caf7ea484cfb69eed7be4c330e825e821991b2
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-04 02:06:41 -0500

    Curiosidades

---
+++ curiosidades/DoubleOSimples.py
@@ -0,0 +1,4 @@
+# He tenido problemas con el interprete al usar comillas dobles y aveces 
+# problemas al usar problemas simples 
+
+#fuente : https://recursospython.com/guias-y-manuales/comillas-dobles-o-simples/
