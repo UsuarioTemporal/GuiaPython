1e4e5224e3dfdfbf53dcc8e587fac0be44249fd7
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-04 12:18:34 -0500

    Teoria de python

--- curiosidades/DoubleOSimples.py
+++ curiosidades/DoubleOSimples.py
@@ -4,3 +4,16 @@ import this
 
 #fuentes : https://recursospython.com/guias-y-manuales/comillas-dobles-o-simples/
 #          http://recursospython.com/guias-y-manuales/zen-de-python-tim-peters/
+#          http://www.recursospython.com/pep8es.pdf
+
+
+# Según la documentacion , para aquellos modulos que expresan ua funcion deben ser declarados por 
+# comillas triples
+
+def suma() : 
+    a,b=1,2
+    """Esto retornara la los dos valores como tupla"""
+    return a,b
+
+"""Sumando los valores de la tupla"""
+print(sum(suma()))
