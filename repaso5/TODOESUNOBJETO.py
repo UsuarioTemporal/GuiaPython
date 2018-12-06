06219e9e453254e4c9e20b62d84db81af0755f7e
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-06 14:33:11 -0500

    POO

--- repaso5/TODOESUNOBJETO.py
+++ repaso5/TODOESUNOBJETO.py
@@ -1,5 +1,22 @@
 class Objeto() : 
     pass
+class OtroObjeto :
+    pass
+print("Objetos : ")
+print(type(OtroObjeto))
+print(type(Objeto))
 
-o = Objeto()
-print(type(o))
+obejto = Objeto() #instanciando una clase 
+clase = Objeto # asignando clase
+
+print("Objeto de la clase propia creada")
+print(type(obejto))
+print("Igualando clases")
+print(type(clase))
+
+
+# Usando la clase OtroObjeto
+otroObjeto = OtroObjeto() #instanciando otro objeto
+claseoOtroObejto = OtroObjeto
+print(type(otroObjeto))
+print(type(claseoOtroObejto))
