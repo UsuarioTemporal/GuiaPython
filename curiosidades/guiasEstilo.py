1f8f01ab5c99e09fb6b6f59fa74f93f0555dd574
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-05 20:13:24 -0500

    Guias de estilo python

---
+++ curiosidades/guiasEstilo.py
@@ -0,0 +1,19 @@
+"""Para programar en python existe convenciones
+en la escritura del código"""
+
+# estas convenciones se usan para la legibilidad 
+# del código y hacerlo entendible y consistente 
+
+
+"""
+ Citando del documenento PEP 8 : 
+    Una guía de estilo se trata de consistencia. La consistencia con esta
+    guía es importante. La consistencia dentro de un proyecto es más
+    importante. La consistencia dentro de un módulo o función es aún
+    más importante.
+
+"""
+
+
+# Razones por la cual romper las reglas de la consistencia en python u otro lenguaje
+
