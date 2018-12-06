371ec262d93147d36f0a3ec36f3ed9911c38d077
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-06 14:56:11 -0500

    Información de los asteriscos

---
+++ curiosidades/asteriscos.py
@@ -0,0 +1,2 @@
+# Existe dos tipos importantes al programar en python los asteriscos dobles y los asteriscos simples
+# estos se utilizan en los parametros de las funciones y tambien en los argumentos de las funciones
