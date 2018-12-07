37ca51022e328160bb3521bbeb7f650dc972f358
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-06 19:14:24 -0500

    POO

--- repaso5/POO.py
+++ repaso5/POO.py
@@ -1,25 +1,25 @@
 class Auto() : 
-    largoChasis = 250
-    anchoChasis = 120 
-    ruedas = 4
-    enMarcha=False
+    def __init__(self): # constructor
+        self.__largoChasis = 250 # el guion abajo significa encapsulado "privado"
+        self.__anchoChasis = 120 
+        self.__ruedas = 4
+        self.__enMarcha=False
 
-    def arrancar(self): # el self hace referencia al propio objeto perteneciente 
+    def arrancar(self,marchando): # el self hace referencia al propio objeto perteneciente 
                         #a la clase , con compraciones con otros lenguajes el self es igual
                         # que el this
-        self.enMarcha=True
-
-    def estado(self) : 
-        if self.enMarcha : 
+        self.__enMarcha=marchando
+        if self.__enMarcha :
             return "Encendido"
         else : 
             return "Apagado"
-
+    def estado(self) : 
+        print(self.__anchoChasis,self.__largoChasis,self.__ruedas,self.__enMarcha)
 
 toyota = Auto()
-toyota.arrancar()
-print(toyota.largoChasis)
-print(toyota.anchoChasis)
-print(toyota.ruedas)
-print(toyota.enMarcha)
-print(toyota.estado())
+sisan = Auto()
+
+print(toyota.arrancar(False))
+toyota.estado()
+print(sisan.arrancar(True))
+sisan.estado()
