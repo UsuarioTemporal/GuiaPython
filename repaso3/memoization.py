5e488b8502f8ab1c68598f56ad90fbebe01bb065
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-15 16:03:20 -0500

    Fibonacci recursivo optimo

--- repaso3/memoization.py
+++ repaso3/memoization.py
@@ -2,12 +2,22 @@
 # -*- coding:utf-8 -*-
 import time
 
-def fib(num):
+def fib(num): #funcion ineficiente
     if num ==1 or num==2 :  
         return num-1
     else :
         return fib(num-1) + fib(num-2)
+
+#funcion eficiente
+cache = {}
+def fibo(num):
+    if num ==1 or num==2 :  
+        return num-1
+    if num not in cache :
+        cache[num]= fibo(num-1)+fibo(num-2)
+    return cache[num]
 start = time.time()
-print(fib(20))
+# print(fib(34))
+print(fibo(98))
 end= time.time()
 print("El tiempo en segundos de ejecucion es {} segundos".format(end-start))
