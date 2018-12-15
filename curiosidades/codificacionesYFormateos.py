f5182d534a305615265c7c485b54c6a75f3c43af
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-15 14:03:49 -0500

    Formateos de variables

--- curiosidades/codificacionesYFormateos.py
+++ curiosidades/codificacionesYFormateos.py
@@ -3,3 +3,7 @@
 """
     comentarios magicos nos ayudan a definir el tipo de codiificacion
 """
+print("La mejor forma de formatear las variables es  : ")
+string = "Texto"
+numero= 2
+print("Soy un texto %s y un numero %d " %(string,numero))
