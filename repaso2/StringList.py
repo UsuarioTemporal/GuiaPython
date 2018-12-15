fe905a3a40c577d18dfb20f5d7f6736a3f33e733
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-15 04:01:41 -0500

    Terminando string

--- repaso2/StringList.py
+++ repaso2/StringList.py
@@ -9,3 +9,13 @@ print(string[-1::-6]) # inicio : final : saltos
 print(string[::-1])
 print(string[-6::])
 print(string[0:7:2])
+
+# convertir string a minusculas
+#string=string.lower()
+#convertir string a mayuscular
+#string=string.upper()
+#result=string.find('c')
+#result = string.count('c')
+#newString = string.replace('c',''x')
+#newString=string.split(' ') esto separa la cadenas en blokes respecto a los espacios
+# y esto retornara una lista
