cda063e52e8309f3a91740d16441f56869c2587e
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-15 02:43:15 -0500

    Agregando nuevas funciones de decoradores felxibles

---
+++ repaso3/decorador3.py
@@ -0,0 +1,20 @@
+def decorator(isValid=True) :
+    def _decorator(func) :
+        def beforeAction():
+            print("Antes de la accion")
+        def afterAction():
+            print("Despues de la accion")
+        def newFunction(*args):
+            if isValid :
+                beforeAction()
+            
+            return func(*args)
+        afterAction()
+        return newFunction
+    return _decorator
+
+@decorator(False)
+def suma(numero1=0,numero2=0):
+    return numero1+numero2
+
+print(suma())
