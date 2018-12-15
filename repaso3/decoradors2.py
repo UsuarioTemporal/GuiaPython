643214b5e03fe5562d30ee42dd48f4200c440f63
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-15 02:06:38 -0500

    Decoradobles flexibles retornables

--- repaso3/decoradors2.py
+++ repaso3/decoradors2.py
@@ -8,3 +8,17 @@ def suma(numer1,numero2):
     print(numer1+numero2)
 
 suma(7,2)
+
+#######################################
+def decorator(func):
+    def newFunction(*args):
+        res=func(*args)
+        return res
+    return newFunction
+
+@decorator
+def suma(var1,var2):
+    return var1+var2
+
+res = suma(7,8)
+print(res)
