23a5a89c2f42018bec6fadc1aa0dc1b286275f26
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-15 16:32:47 -0500

    Terminando decoradores

--- repaso3/decorandoFuncionFibonacci.py
+++ repaso3/decorandoFuncionFibonacci.py
@@ -1,9 +1,19 @@
+import time 
 def memoize(func):
     cache={}
     def wrapper(*args,**kwargs):
-        pass
+        if args not in cache :
+            cache[args]=func(*args,**kwargs)
+        return cache[args]
     return wrapper
 
 @memoize
 def fib(num):
-    return num
+    if num ==1 or num==2 :  
+        return num-1
+    return fib(num-1) + fib(num-2)
+
+start =time.time()
+print(fib(100))
+end=time.time()
+print("El tiempo en segundos de ejecucion es {} segundos".format(end-start))
