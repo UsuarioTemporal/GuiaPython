205b5b8d6783cb51b9d947c0f87ab0662808f1bc
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-14 23:41:13 -0500

    Comentario de que es lo que hace una funcion decoradora

--- repaso3/decoradores.py
+++ repaso3/decoradores.py
@@ -1,7 +1,8 @@
 # los decoradores son funciones que reciben como parametro
 #una funcion para poder retornar otra funcion
 def decorador(func): 
-    def nueva_funcion():
+    def nueva_funcion(): # esta funcion se encargara de ejecutar la funcion que 
+                        # recibe como parametro
         pass
     return nueva_funcion()
 
