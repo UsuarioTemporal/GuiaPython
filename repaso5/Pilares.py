b88acf1c1d0ae05a6f4ec321841ed33151023c8b
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-25 04:30:09 -0500

    Polimorfismo

--- repaso5/Pilares.py
+++ repaso5/Pilares.py
@@ -3,3 +3,6 @@
         un objeto de la subclase , siempre sera un objeto de la superclase
         Es decir que un que un Empleado es simpre una Persona
 """
+thom=Empleado()
+isinstance(thom,Empleado) # True
+isinstance(thon,Persona) # True
--- repaso6/hilos.py
+++
@@ -1,5 +0,0 @@
-"""
-    Principio de sustitución : 
-        un objeto de la subclase , siempre sera un objeto de la superclase
-        Es decir que un que un Empleado es simpre una Persona
-"""
