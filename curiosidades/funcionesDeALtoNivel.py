77a562476c68242da186da1cdeeeeaedc3d5a502
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-24 19:16:05 -0500

    Agregando definicion de funciones de alto nivel

--- curiosidades/funcionesDeALtoNivel.py
+++ curiosidades/funcionesDeALtoNivel.py
@@ -3,3 +3,7 @@
 
     Lo que significa que se puede pasar como variable a otra funcion o se puede anidar funciones
 """
+
+"""
+    Este tema se ve con mucha mas profundidad en los decoradores, los ejemplos de decoradores estan en esta carpeta , revisar :3
+"""
