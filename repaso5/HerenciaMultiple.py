c699a0caf38541e9076d8be727e9f830c32cefed
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-24 23:25:40 -0500

    Propertys

--- repaso5/HerenciaMultiple.py
+++ repaso5/HerenciaMultiple.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+
+
+
+
+
+
+
+
+
 class Email():
     def __init__(self,password):
-        pass
+        self.__password=password
+    @property
+    def password(self):
+        return self.__password
+    @password.setter
+    def password(self,value):
+        self.__password=value
+
+thom =Email('112')
+# thom.getPassword='15'
+thom.password='45'
+print(thom.password)
