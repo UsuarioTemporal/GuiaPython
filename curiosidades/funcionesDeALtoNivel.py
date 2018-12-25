e96364f024b9ad03f94d3cb302b0dae9b791ed3e
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-24 19:13:12 -0500

    Funciones de alto nivel , explicación

--- curiosidades/funcionesDeALtoNivel.py
+++ curiosidades/funcionesDeALtoNivel.py
@@ -1,5 +1,5 @@
 """
     Las funciones en en python son objetos de primera clase,
 
-    Lo que significa que se puede pasar como varibale a otra funcion o se puede anidar funciones
+    Lo que significa que se puede pasar como variable a otra funcion o se puede anidar funciones
 """
