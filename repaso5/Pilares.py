54f9945f469f5fbed4ffc842fe27cfb1e0065de6
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-25 04:38:36 -0500

    Extras en curiosidades

--- repaso5/Pilares.py
+++ repaso5/Pilares.py
@@ -1,8 +1,14 @@
+
 """
     Principio de sustitución : 
         un objeto de la subclase , siempre sera un objeto de la superclase
         Es decir que un que un Empleado es simpre una Persona
 """
-thom=Empleado()
-isinstance(thom,Empleado) # True
-isinstance(thon,Persona) # True
+# thom=Empleado()
+# isinstance(thom,Empleado) # True
+# isinstance(thon,Persona) # True
+
+
+"""
+    EL objeto se adpatara al tipo de instancia
+"""
