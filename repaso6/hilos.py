dc908924d2da188005f29e38f84f759792bad6ba
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-26 10:45:51 -0500

    Parecido a java

--- repaso6/hilos.py
+++ repaso6/hilos.py
@@ -4,6 +4,19 @@ import datetime
 import logging
 
 def consultar(id_persona):
-    pass
+    time.sleep(2)
+    
 def guardar(id_persona,data):
-    pass
+    time.sleep(5)
+    
+
+start=datetime.datetime.now();
+t1 = threading.Thread(name="hilo_1",target=consultar,args=(1, ))
+t2 = threading.Thread(name="hilo_2",target=guardar,args=(1, "Thom"))
+
+t1.start()
+t2.start()
+
+end=datetime.datetime.now()
+
+print("El tiempo transcurrido es {} ".format(end-start))
