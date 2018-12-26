092200f7eb2c3f7273128bf2958fb14cdb83c55d
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-25 19:05:37 -0500

    Modulos

--- curiosidades/modulos.py
+++ curiosidades/modulos.py
@@ -1,5 +1,18 @@
+import monedas
 import math
+
 x= 5.74
 print(math.floor(x))
 print(math.ceil(x))
 print(math.trunc(x))
+
+print("Ingrese tipo de moneda y el valor de cambio [separados por una coma ,]",end=" ")
+texto = str(input()).split(",")
+tipoDeMoneda = texto[0]
+valorCambio=float(texto[1])
+
+monedas.definirTipoMoneda(tipoDeMoneda,valorCambio)
+print("Ingrese el monto a convertir ",tipoDeMoneda," a USD ",end=" ")
+monto=float(input())
+resultado = monedas.convertir(monto,tipoDeMoneda)
+print(monto," ",tipoDeMoneda," en USD es ",resultado)
--- curiosidades/monedas.py
+++ curiosidades/monedas.py
@@ -1,1 +1,11 @@
 divisas = {"PEN":3.256,"MXM":20}
+
+def definirTipoMoneda(tipoMoneda,valor) :
+    divisas[tipoMoneda]=valor
+    return
+
+def convertir(montoInicial,tipoMoneda="PEN") :
+    valor=divisas.get(tipoMoneda)
+    montoInicial/=valor
+    return montoInicial
+
