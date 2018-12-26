11351542ed0a1026bb620ad31ce44fbe744ec550
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-26 10:16:50 -0500

    Métodos abstractos

--- repaso5/metodosAbstractos.py
+++ repaso5/metodosAbstractos.py
@@ -1,1 +1,23 @@
 from abc import ABCMeta , abstractmethod
+
+#interface
+class Figura(object):
+    __metaclass__=ABCMeta
+
+    @abstractmethod
+    def area(self):pass
+        
+    @abstractmethod
+    def perimetro(self) :pass
+
+class Rectangulo(Figura):
+    def __init__(self,ancho,altura):
+        self.__ancho=ancho
+        self.__altura=altura
+        super(Rectangulo,self).__init__()
+
+    def area(self):
+        return self.__altura*self.__ancho
+    def perimetro(self):
+        return self.__altura*2 + self.__ancho*2
+cuadrado=Rectangulo(45,4)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+#paquetes distribuibles
+setup(
+    name="paquete",
+    version="1.0.0",
+    description="paquete de repasos",
+    author="Thom",
+    author_email="thomtwd@gmail.com",
+    url="https://github.com/UsuarioTemporal/RepasoPython",
+    package=["repaso5","package.operaciones"]
+)
