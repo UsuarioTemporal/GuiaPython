241de139fff99136125104084c95507be5a262ab
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-26 10:57:41 -0500

    ejecuciones

--- repaso6/hilos.py
+++ repaso6/hilos.py
@@ -2,21 +2,24 @@ import threading
 import time
 import datetime
 import logging
-
+"""
+    Por defecto python al igual que java tiene un hilo implicito que es el encargado de la ejecución , entonces
+    el total es de hilos son 3 , entonces si queremos que ele hilo que ejcute se sincronize con los demas para los demas cumplan su funcion y luego al terminar cada uno , termine e hilo del main
+"""
 def consultar(id_persona):
     time.sleep(2)
     
 def guardar(id_persona,data):
     time.sleep(5)
-    
 
-start=datetime.datetime.now();
+
+start=time.time()
 t1 = threading.Thread(name="hilo_1",target=consultar,args=(1, ))
 t2 = threading.Thread(name="hilo_2",target=guardar,args=(1, "Thom"))
 
-t1.start()
-t2.start()
+t1.start() #comenzando el hilo 1 
+t2.start() #comenzando el hilo 2
 
-end=datetime.datetime.now()
+end=time.time()
 
 print("El tiempo transcurrido es {} ".format(end-start))
