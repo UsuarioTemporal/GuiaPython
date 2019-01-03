556a2e80b564399d317c54a678a0cef86f8cfbd8
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-03 11:34:57 -0500

    Operaciones con la BD

--- conexionDB/mysql.py
+++ conexionDB/mysql.py
@@ -3,3 +3,9 @@ import pymysql
 # Mira mama, mas facil que java imposible
 db =pymysql.connect("localhost", "root", "mysql","tienda_java")
 print(db)
+
+
+
+cursor = bd.cursor()
+
+cursor.execute("SELECT * FROM usuarios")
