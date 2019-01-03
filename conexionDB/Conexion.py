c9fc9f656c3ea974274eb5a34ae70d9022c51e2c
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-03 12:09:29 -0500

    Clase conexion al igual que java

--- conexionDB/Conexion.py
+++ conexionDB/Conexion.py
@@ -1,4 +1,21 @@
 import pymysql
 
 class Conexion() :
-    pass
+    def __init__(self,servidor,usuario,clave,bd):
+        self.db = pymysql.connect(servidor,usuario,clave,bd)
+        self.cursor = self.bd.cursor()
+        print("Conexion exitosa")
+    
+    def ejecutarQuery(self,sql):
+        self.cursor.execute(sql)
+        return self.cursor
+
+    def cerrarConexion(self):
+        self.db.close()
+        print("Base de datos cerrada")
+    
+    def commit(self):
+        self.bd.commit()
+    
+    def roolBack(self):
+        self.bd.rollback()
