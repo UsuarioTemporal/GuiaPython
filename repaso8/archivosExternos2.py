4fcaa465dbf46a8c2117e5129c3e13c785d305ad
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-04 14:15:49 -0500

    Seroalizacion

--- repaso8/archivosExternos2.py
+++ repaso8/archivosExternos2.py
@@ -1,7 +1,8 @@
 from io import open
 
-archivo = open("repaso8/archivo.txt","r")
+archivo = open("repaso8/archivo.txt","r") # PARA LEER Y ESCRIBIR SE USARÁ r+
 
 print(archivo.read())
 archivo.seek(0)
 print(archivo.readlines())
+
---
+++ repaso9/serializacion.py
@@ -0,0 +1,3 @@
+"""
+    Consiste en guardar en un fichero externo una coleccion o un objeto , pero lo mas importante es que esto se guardara en un archivo coidficado , encriptado (codigo binario)
+"""
