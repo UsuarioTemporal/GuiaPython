34ac78d67a72ce9a7acb315b11b0fa8bfb0cfd95
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-03 20:27:30 -0500

    Metodos de representacion y de cadena}

--- curiosidades/__str__And__Rep__.py
+++ curiosidades/__str__And__Rep__.py
@@ -1,8 +1,20 @@
 """
-
+    repr debería devolver una cadena quepuede copiarse y pegase para construtir el estado exacto del objeto, mientras el str es útil para logginf y observing resultados de depuracion.
 """
 
 from decimal import Decimal
 a = Decimal(1.25)
 print(str(a)) # muestra a el objeto en formato string
 print(repr(a)) # muestra la representacion del objeto
+
+
+
+class Vector:
+    def __init__(self,*args):
+        self.x=args[0]
+        self.y=args[1]
+        self.z=args[2]
+    def __repr__(self):
+        return 
+    def __str__(self):
+        return 
