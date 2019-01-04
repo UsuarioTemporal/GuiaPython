5da38632fcfa97a06ee5058286ec9836152c9768
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-03 20:21:39 -0500

    Representacion

---
+++ curiosidades/__str__And__Rep__.py
@@ -0,0 +1,8 @@
+"""
+
+"""
+
+from decimal import Decimal
+a = Decimal(1.25)
+print(str(a)) # muestra a el objeto en formato string
+print(repr(a)) # muestra la representacion del objeto
---
+++ curiosidades/clasesInternas.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+
+class Perro : 
+    def __init__(self):
+        self.nombre = "asdasd"
+
+prro = Perro()
+print(prro.__str__)
