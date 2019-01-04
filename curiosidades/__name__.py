464a283615f5aaaebce0cebdaaa703e866d82927
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-03 21:51:09 -0500

    Condicional para el flujo de modulos

---
+++ curiosidades/__name__.py
@@ -0,0 +1,8 @@
+def saludar():
+    print("Hola")
+def resolver():
+    print("resulvo")
+
+if __name__=='__main__': 
+    saludar()
+    resolver()
--- curiosidades/__str__And__Rep__.py
+++ curiosidades/__str__And__Rep__.py
@@ -1,20 +1,37 @@
 """
-    repr debería devolver una cadena quepuede copiarse y pegase para construtir el estado exacto del objeto, mientras el str es útil para logginf y observing resultados de depuracion.
+    repr debería devolver una cadena que puede copiarse y pegase para construtir el estado exacto del objeto, mientras el str es útil para logginf y observing resultados de depuracion.
 """
 
 from decimal import Decimal
+
+
+
+
+class Vector:
+    def __init__(self,args):
+        self.x=args[0]
+        self.y=args[1]
+        self.z=args[2]
+    
+    def __str__(self):
+        return f"x: {self.x!r}, y: {self.y!r}, z: {self.z!r}"
+
+    def __repr__(self):
+        return "Vector3([{},{},{}])".format(self.x, self.y, self.z)
+
+
+if __name__=='__main' : #es este el script principal?
+    pass
+
+"""
+    __repr__ : La representacion del objeto python generalmente lo convertira de nuevo a ese objeto
+"""
+
 a = Decimal(1.25)
 print(str(a)) # muestra a el objeto en formato string
 print(repr(a)) # muestra la representacion del objeto
 
 
-
-class Vector:
-    def __init__(self,*args):
-        self.x=args[0]
-        self.y=args[1]
-        self.z=args[2]
-    def __repr__(self):
-        return 
-    def __str__(self):
-        return 
+v = Vector([1,2,3])
+print(repr(v))
+print(str(v))
