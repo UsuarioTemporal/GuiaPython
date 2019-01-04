3a0c5dabf8102e2d9d87d919e12657f623e93f52
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-04 14:49:37 -0500

    Creando codigo binarios

--- repaso9/serializacion.py
+++ repaso9/serializacion.py
@@ -1,3 +1,9 @@
 """
-    Consiste en guardar en un fichero externo una coleccion o un objeto , pero lo mas importante es que esto se guardara en un archivo coidficado , encriptado (codigo binario)
+    Consiste en guardar en un fichero externo una coleccion o un objeto , pero lo mas importante es que esto se guardara en un archivo codificado , encriptado (codigo binario)
 """
+import pickle
+listaNombres = ["Pedro","Ana","Maria","Isabel"]
+ficheroBinario = open("repaso9/listaNombre","wb")
+pickle.dump(listaNombres,ficheroBinario)
+ficheroBinario.close()
+del (ficheroBinario)
