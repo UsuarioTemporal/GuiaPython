36ba2264ca1e0cd3c72208f54293e74e4717ab63
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-04 20:05:49 -0500

    IZI

--- conexionDB_2/operacionesBasicas.py
+++ conexionDB_2/operacionesBasicas.py
@@ -1,1 +1,12 @@
 from conexionDB import Conexion
+
+try:
+    cx = Conexion.Conexion("localhost", "root", "mysql","tienda_java")
+    sql = "CREATE TABLE IF NOT EXISTS producto(id_producto INT PRIMARY KEY AUTO_INCREMENT,nombre_producto VARCHAR(20) NOT NULL,precio_producto DECIMAL(10,2) NOT NULL)"
+    cx.ejecutarQuery(sql)
+    cx.commit()
+except Exception as ex:
+    print(ex.__dict__)
+finally :
+    cx.cerrarConexion()
+
