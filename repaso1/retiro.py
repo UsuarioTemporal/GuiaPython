0e230eaf7310801fdbd0eb38c8da28e8ea021c92
UsuarioTemporal <thomtwd@gmail.com>
2019-04-09 01:22:12 -0500

    Planteamiento de cajero

---
+++ repaso1/retiro.py
@@ -0,0 +1,16 @@
+class Cajero :
+
+    def __init__(self,password=''):
+        self.password=password
+        self.dollarClass = [i  for i in range(0,105,5) ]
+        self.dollarClass[0]=1
+    def check(self):
+        return self.password == '12345'
+    def distribute (dollar):
+        pass
+
+cajero1 = Cajero('12345')
+if cajero1.check(): 
+    print(cajero1.dollarClass)
+else :
+    print('No ingreso')
