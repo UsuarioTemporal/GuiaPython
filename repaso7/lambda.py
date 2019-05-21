01eb84cfcc6855b875a7c33e9a56fe54cf14a3f2
UsuarioTemporal <thomtwd@gmail.com>
2019-05-21 01:53:02 -0500

    Expresion lambda para un formulario simple

--- repaso7/lambda.py
+++ repaso7/lambda.py
@@ -7,3 +7,7 @@ g= lambda x :3*x+1
 print(g(3))
 
 #supongamos que necesitamos conbinar los nombres y apellidos de un un formulario
+
+full_name = lambda fn,ln : fn.strip().title()+' '+ln.strip().title()
+
+print(full_name('thom ', ' roman '))
