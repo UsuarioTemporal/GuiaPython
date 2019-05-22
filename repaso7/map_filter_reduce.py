48b25dfe15485e38afe91f2327b6db392efb11fb
UsuarioTemporal <thomtwd@gmail.com>
2019-05-21 20:29:16 -0500

    Filter

--- repaso7/map_filter_reduce.py
+++ repaso7/map_filter_reduce.py
@@ -1,4 +1,5 @@
 import math
+import statistics
 
 def area(radius):
     return math.pi*(radius**2)
@@ -22,3 +23,23 @@ newTemps = list(map(lambda tp : (tp[0],(9/5)*tp[1] + 32),temps))
 
 print('Celcius',temps)
 print('Fahrenheit',newTemps)
+
+############################################################
+
+
+data = [1.3,2.7,0.8,4.1,4.3,-0.1]
+
+avg = statistics.mean(data)
+
+# valores que estan por encima de la media
+newData = list(filter(lambda element:element>avg,data))
+print(newData)
+
+# Remove missing data
+
+countries = ['','Peru','Argentina','','Brazil','','']
+
+newContries = list(filter(None,countries)) # Esto filtrará todos los valores que se tratan como falsos en una configración booleana
+# newContries = list(filter(lambda country :country is not '',countries))
+
+print(newContries)
