2b2625cc420367238d682c9d4a4e76884fc806e7
UsuarioTemporal <thomtwd@gmail.com>
2019-05-21 22:08:50 -0500

    HAciendo un hashmap con el id del usuario

--- repaso7/makeHashMap.py
+++ repaso7/makeHashMap.py
@@ -1,6 +1,15 @@
 import json
-
+from functools import reduce
 # with open('db.json') as file :
 #     data=json.load(file)
 data=[dic for dic in json.load(open('db.json'))]
-print(data)
+# print(data)
+
+def makeHash(users,key):
+    def analized(dic,person):
+        dic[person[key]]=person
+        return dic
+    return reduce(analized,users,{})
+
+hashMap= makeHash(data,'id')
+print(hashMap)
